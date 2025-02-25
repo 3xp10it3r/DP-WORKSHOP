00e5ee7b48084f86a3dfbfc3d3ae4df0034396f5
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-02-26 00:52:19 +0530

    DP way

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -26,3 +26,36 @@ public:
         return rec(0, n);
     }
 };
+
+
+// DP Way
+
+class Solution {
+public:
+    int dp[50]; // max level
+
+    int rec(int level, int n) {
+        // Basecase
+        if(level == n) // if reached nth stair then 1 way add
+            return 1;
+
+        if(dp[level] != -1)
+            return dp[level];
+        
+        int ans = 0;
+        // explore choices
+        for(int step = 1; step <= 2; step++) {
+            if(level + step <= n) { // check 
+                ans += rec(level + step, n);
+            }
+        }
+
+        // return 
+        return dp[level] = ans;
+    }
+
+    int climbStairs(int n) {
+        memset(dp, -1, sizeof(dp));
+        return rec(0, n);
+    }
+};
