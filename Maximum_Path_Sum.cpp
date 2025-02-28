bb9d7f8c0b66432124389b857d4835e072cadd87
Digvijay Gupta <65729055+3xp10it3r@users.noreply.github.com>
2025-03-01 01:15:56 +0530

    Create Maximum_Path_Sum.cpp

---
+++ Maximum_Path_Sum.cpp
@@ -0,0 +1,51 @@
+// Given a m x n grid filled with numbers, find a path from top left to bottom right, which maximizes the sum of all numbers along its path.
+
+// Note: You can only move either down or right at any point in time.
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[1010][1010];
+
+int dp[1020][1020];
+
+int rec(int r, int c) {
+    
+    if(r == 0 && c == 0) {
+        return arr[0][0];
+    }
+    
+    
+    if(dp[r][c] != -1) 
+        return dp[r][c];
+    
+    int ans = -1e9;
+    
+    if(r!=0) {
+        ans = max(ans, arr[r][c] + rec(r-1, c));
+    }
+    if(c!=0) {
+        ans = max(ans, arr[r][c] + rec(r, c-1));
+    }
+    
+    return dp[r][c] = ans;
+}
+
+int main()
+{
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    memset(dp, -1, sizeof(dp));
+    
+    cout << rec(n-1, m-1);
+
+    return 0;
+}
